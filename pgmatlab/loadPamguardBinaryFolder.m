0e584aa849ab95e2007217762beda6dc93568273
Jamie Mac <macster110@gmail.com>
2022-07-21 15:01:36 +0100

    Updates to  load binary folder
    Added ability to set time limits for a loadPAMGuardbinaryFolder.m function.

--- pgmatlab/loadPamguardBinaryFolder.m
+++ pgmatlab/loadPamguardBinaryFolder.m
@@ -1,34 +1,58 @@
-function [data background] = loadPamguardBinaryFolder(dir, fileMask, verbose, filterfun)
+function [data, background] = loadPamguardBinaryFolder(dir, fileMask, verbose, varargin)
+
 % load all data from a folder and it's sub folders
-%  data = loadPamguardBinaryFolder(dir, fileMask, verbose) loads all data from 
+%  data = loadPamguardBinaryFolder(dir, fileMask, verbose) loads all data from
 % the folder dir where files satisfy the fileMask. All files must
-% contain data of the same format so mask must specify a particular type 
+% contain data of the same format so mask must specify a particular type
 % of data file, e.g. clicks*.pgdf. If verbose is a non-zero number 'n' then
-% every n files a progress message will be printed on the temrinal. 
+% every n files a progress message will be printed on the temrinal.
 %
-% e.g. allclicks = loadPAMFolder('C:/Mydata', 'Click_Detector_*.pgdf', 5);
+% e.g. allclicks = loadPamguardBinaryFolder('C:/Mydata', 'Click_Detector_*.pgdf', 5);
 %
 if (nargin < 3)
-    verbose = 0;
+    verbose = 2;
 end
-if nargin < 4
-    filterfun = @passalldata;
+
+timesonly=false;
+if nargin<4
+    iArg = 0;
+    while iArg < numel(varargin)
+        iArg = iArg + 1;
+        switch(varargin{iArg})
+            case 'timesonly'
+                iArg = iArg + 1;
+                timesonly = varargin{iArg};
+        end
+    end
+    
+else
+    % need to do this so that varargin is successfully passed to new
+    % function
+    %     varargin = varargin(1:end);
 end
 data = [];
 background = [];
 d = dirsub(dir, fileMask);
 for i = 1:numel(d)
+    %     disp(d(i).name);
     if verbose
         if mod(i, verbose) == 0
             fprintf('Loading %d/%d (%d) %s\n', i, numel(d), numel(data), d(i).name);
         end
     end
-    [dat fInf] = loadPamguardBinaryFile(d(i).name, 'filter', filterfun);
+    % need to use varargin{:} to pass arguments from one function to
+    % another for some reason.
+    [dat, fInf] = loadPamguardBinaryFile(d(i).name, varargin{:});
+    if timesonly
+        dat = [[dat.UID]; [dat.date]; [dat.type]]; %dates are accurate to 10us
+    end
+    
+
     if ~isempty(dat)
         data = [data dat];
     end
+
     if isfield(fInf, 'background')
         background = [background fInf.background];
     end
 end
-    
