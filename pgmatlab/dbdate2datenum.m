88e60617c409e5740b93913edd24859f6dfb1bb7
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2023-09-28 21:32:45 +0100

    time updates
    update millisToDateNum to check everything is double. Seem to be some rounding errors otherwise.

--- pgmatlab/dbdate2datenum.m
+++ pgmatlab/dbdate2datenum.m
@@ -2,29 +2,30 @@ function [dn millis] = dbdate2datenum(dbdate)
 % function dn = dbdate2datenum(dbdate)
 %  converts a YYYY-MM-DD HH:MM:SS.ms datestring returned from a database
 %  into a datenum (standard Matlab time)
-  if iscell(dbdate)
+if iscell(dbdate)
     n = length(dbdate);
     for i = 1:n
         [aDate, ms] = dbdate2datenum(dbdate{i});
-      dn(i) = aDate;
-          millis(i) = ms;
+        dn(i) = aDate;
+        millis(i) = ms;
     end
     return;
-  end
-    
-  dbdate = deblank(dbdate);
-  dbdate = strrep(dbdate,'''','');
-  % dbdate will be in the format YYYY-MM-DD HH:MM:SS.ms which matlab doesn't understand !
-  YY = str2num(dbdate(1:4));
-  MM = str2num(dbdate(6:7));
-  DD = str2num(dbdate(9:10));
-  HH = str2num(dbdate(12:13));
-  M  = str2num(dbdate(15:16));
-  SS = str2num(dbdate(18:19));
-  MS = 0;
-  if length(dbdate) > 20;
+end
+if isstring(dbdate)
+    dbdate = char(dbdate);
+end
+dbdate = deblank(dbdate);
+dbdate = strrep(dbdate,'''','');
+% dbdate will be in the format YYYY-MM-DD HH:MM:SS.ms which matlab doesn't understand !
+YY = str2num(dbdate(1:4));
+MM = str2num(dbdate(6:7));
+DD = str2num(dbdate(9:10));
+HH = str2num(dbdate(12:13));
+M  = str2num(dbdate(15:16));
+SS = str2num(dbdate(18:19));
+MS = 0;
+if length(dbdate) > 20;
     MS = str2num(dbdate(20:length(dbdate))); % this is read with the . so is fractional seconds
-  end
-  dn = datenum(YY,MM,DD,HH,M,SS) + MS/24/3600;
-  millis = MS*1000;
-  
+end
+dn = datenum(YY,MM,DD,HH,M,SS) + MS/24/3600;
+millis = MS*1000;
