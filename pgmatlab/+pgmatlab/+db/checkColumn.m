9107b861923180b1968c62d19dd6d6eec1f52380
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2025-10-09 12:31:32 +0100

    Update checkColumn.m
    Fix type on function

--- pgmatlab/+pgmatlab/+db/checkColumn.m
+++ pgmatlab/+pgmatlab/+db/checkColumn.m
@@ -7,7 +7,7 @@ if e
 end
 tableName = pgmatlab.utils.charArray(tableName);
 columnName = pgmatlab.utils.charArray(columnName);
-format = pgmatlab.utils.charArray(columformatnName);
+format = pgmatlab.utils.charArray(columnName);
 cmd = sprintf('ALTER TABLE %s ADD COLUMN %s %s', tableName, columnName, format);
 ans = exec(con, cmd);
 e = pgmatlab.db.columnExists(con, tableName, columnName);
